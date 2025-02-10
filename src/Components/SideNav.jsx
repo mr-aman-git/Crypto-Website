7fb36936ec687ad52af43e2fee6a38a9c743e24e
mr-aman-git <amanazr2003@gmail.com>
2025-02-10 11:24:28 +0530

    sidenav complete

--- src/Components/SideNav.jsx
+++ src/Components/SideNav.jsx
@@ -6,16 +6,22 @@ const SideNav = () => {
     <div className="sidenav">
         <div className="nav">
 
-            <div className="logo">
-                <img src="https://crypto.com/__assets/mkt-nav-footer/images/cdc_logo.svg" alt="" />
+            <div className="mininav">
+                <div className="logo">
+                    <img src="https://crypto.com/__assets/mkt-nav-footer/images/cdc_logo.svg" alt="" />
+                </div>
+
+                <div className="dashboard hovers">
+                    <p> <i class="ri-dashboard-line"></i> Dashboard</p>
+                </div>
+
+                <div className="transaction hovers">
+                    <p> <i class="ri-arrow-left-right-fill"></i> Transaction</p>           
+                </div>
             </div>
 
-            <div className="dashboard">
-                <p>Dashboard</p>
-            </div>
-
-            <div className="transaction">
-                <p>Transaction</p>           
+            <div className="support hovers">
+                <p> <i class="ri-customer-service-2-line"></i> Support</p>           
             </div>
 
         </div>
