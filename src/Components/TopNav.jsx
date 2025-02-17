e5172de40760aa2d143cfdce430a722defc57cd8
mr-aman-git <amanazr2003@gmail.com>
2025-02-17 21:53:20 +0530

    some change

--- src/Components/TopNav.jsx
+++ src/Components/TopNav.jsx
@@ -1,39 +1,49 @@
-import React from 'react'
-import './TopNav.css';
-import Logo from '../assets/logos.png'
+import React from "react";
+import "./TopNav.css";
+import Logo from "../assets/logos.png";
 
 function TopNav() {
   return (
     <>
-    <nav>
+      <nav>
         <div className="top-nav">
+          <div className="logo">
+            <img src={Logo} alt="" />
+          </div>
 
-            <div className="logo">
-                <img src= {Logo} alt="" />
+          <div className="nav-box">
+            <div className="dash">
+              <p>Dashboard</p>
             </div>
-            
-            <div className="nav-box">
-                <div className="dash">
-                 <p>Dashboard</p>
-                </div>
 
-                <div className="user">
+            <div class="dropdown">
+              <button class="dropbtn">Dropdown</button>
+              <div class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+            </div>
+
+            
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default TopNav;
+
+{
+  /* <div className="user">
                  <p><i class="ri-user-community-line"></i>
+
                   <ul>
                     <li>Profile</li>
                     <li>Detail</li>
                     <li>Payment</li>
                   </ul>
                   </p>
-                </div>
-
-            </div>
-
-        </div>
-
-    </nav>
-    </>
-  )
+                </div> */
 }
-
-export default TopNav
